3b5fc460feea7a3d0688dd06b6a401055ff2decc
alec <alec@DESKTOP-NP95TT9>
2020-01-15 11:16:15 -0800

    Added Evaluator Code

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -1,8 +1,48 @@
 ﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace FormulaEvaluator
 {
     public class Class1
     {
+        public delegate int Lookup(String variable_name);
+
+        public static int Evaluate(String expression, Lookup variableEvaluator)
+        {
+
+            string[] substrings = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+            Stack operation = new Stack();
+            Stack value = new Stack();
+
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                if (substrings[i].Equals("("))
+                {
+                    operation.Push("(");
+                    int temp = i + 1;
+                    while (!substrings[temp].Equals(")"))
+                    {
+                        if (!substrings[temp].Equals("(-)|(\\+)|(\\*)|(/)"))
+                        {
+                            value.Push(substrings[temp]);
+                        }
+                        if (substrings[temp].Equals("(\\*)|(/)"))
+                        {
+                            operation.Push(substrings[temp]);
+                        }
+                        if (substrings[temp].Equals("(-)|(\\+)"))
+                        {
+                            operation.Push(substrings[temp]);
+                        }
+                        temp++;
+                    }
+                    operation.Push(")");
+                    i = temp + 1;
+                }
+            }
+
+            return 0;
+        }
     }
 }
