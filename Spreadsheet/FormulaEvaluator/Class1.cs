3b54f364060fa89d49c4d1754c5bac422ea22874
alec <alec@DESKTOP-NP95TT9>
2020-01-16 14:01:49 -0800

    almost done just have some few errors to fix

--- Spreadsheet/FormulaEvaluator/Class1.cs
+++ Spreadsheet/FormulaEvaluator/Class1.cs
@@ -1,48 +1,150 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace FormulaEvaluator
 {
-    public class Class1
+    public class Evaluator
     {
         public delegate int Lookup(String variable_name);
 
         public static int Evaluate(String expression, Lookup variableEvaluator)
         {
-
             string[] substrings = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
-            Stack operation = new Stack();
-            Stack value = new Stack();
-
+            Stack<String> operation = new Stack<String>();
+            Stack<int> value = new Stack<int>();
             for (int i = 0; i < substrings.Length; i++)
             {
+                if (int.TryParse(substrings[i], out int convert))
+                {
+                    if (value.Count == 0)
+                    {
+                        value.Push(convert);
+                    }
+                    if (operation.Peek().Equals("*"))
+                    {
+                        int popped = value.Pop();
+                        int tempVal = convert * popped;
+                        value.Push(tempVal);
+                        operation.Pop();
+                    }
+                    if (operation.Peek().Equals("/"))
+                    {
+                        if (convert == 0)
+                        {
+                            throw new ArgumentException("Cannot divide zero");
+                        }
+                        int popped = value.Pop();
+                        int tempVal = convert / popped;
+                        value.Push(tempVal);
+                        operation.Pop();
+                    }
+                }
+                if (substrings[i].Equals("+"))
+                {
+                    if (operation.Peek().Equals("+") && value.Count >= 2)
+                    {
+                        operation.Pop();
+                        int tempVal = value.Pop() + value.Pop();
+                        value.Push(tempVal);
+                        operation.Push("+");
+                    }
+                    if (operation.Peek().Equals("-") && value.Count >= 2)
+                    {
+                        operation.Pop();
+                        int tempVal = value.Pop() - value.Pop();
+                        value.Push(tempVal);
+                        operation.Push("+");
+                    }
+                    else
+                    {
+                        operation.Push("+");
+                    }
+                }
+             
+                if (substrings[i].Equals("-"))
+                {
+                    if (operation.Peek().Equals("+") && value.Count >= 2)
+                    {
+                        operation.Pop();
+                        int tempVal = value.Pop() + value.Pop();
+                        value.Push(tempVal);
+                        operation.Push("-");
+                    }
+                    if (operation.Peek().Equals("-") && value.Count >= 2)
+                    {
+                        operation.Pop();
+                        int tempVal = value.Pop() - value.Pop();
+                        value.Push(tempVal);
+                        operation.Push("-");
+                    }
+                    else
+                    {
+                        operation.Push("-");
+                    }
+                }
                 if (substrings[i].Equals("("))
                 {
                     operation.Push("(");
-                    int temp = i + 1;
-                    while (!substrings[temp].Equals(")"))
-                    {
-                        if (!substrings[temp].Equals("(-)|(\\+)|(\\*)|(/)"))
-                        {
-                            value.Push(substrings[temp]);
-                        }
-                        if (substrings[temp].Equals("(\\*)|(/)"))
-                        {
-                            operation.Push(substrings[temp]);
-                        }
-                        if (substrings[temp].Equals("(-)|(\\+)"))
-                        {
-                            operation.Push(substrings[temp]);
-                        }
-                        temp++;
-                    }
-                    operation.Push(")");
-                    i = temp + 1;
                 }
-            }
 
-            return 0;
+
+                if (substrings[i].Equals(")"))
+                {
+                    if (operation.Peek().Equals("+") || operation.Peek().Equals("-"))
+                    {
+
+                        if (operation.Peek().Equals("+") && value.Count >= 2)
+                        {
+                            operation.Pop();
+                            int tempVal = value.Pop() + value.Pop();
+                            value.Push(tempVal);
+                        }
+                        if (operation.Peek().Equals("-") && value.Count >= 2)
+                        {
+                            operation.Pop();
+                            int tempVal = value.Pop() - value.Pop();
+                            value.Push(tempVal);
+                        } 
+                    }
+                    if (operation.Peek().Equals("("))
+                    {
+                        operation.Pop();
+                    }
+                    else
+                    {
+                        throw new ArgumentException("A ( isnt found where expected");
+                    }
+                    if(operation.Peek().Equals("*")|| operation.Peek().Equals("/"))
+                    {
+                        if (operation.Peek().Equals("*") && value.Count >= 2)
+                        {
+                            operation.Pop();
+                            int tempVal = value.Pop() * value.Pop();
+                            value.Push(tempVal);
+                        }
+                        if (operation.Peek().Equals("/") && value.Count >= 2)
+                        {
+                            operation.Pop();
+                            int tempVal = value.Pop();
+                            if(value.Peek()==0)
+                            {
+                                throw new ArgumentException("cannot be divided by zero");
+                            }
+                            int tempVal2 = value.Pop();
+                            int tempResult = tempVal / tempVal2;
+                            value.Push(tempResult);
+                        }
+                    }
+
+                }
+                else
+                    throw new ArgumentException("unknown value inserted");
+
+
+            }
+            return value.Pop();
         }
     }
 }
